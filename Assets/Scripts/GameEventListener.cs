96157267b8445a8dfaf2c9aa9d13c12e47102b4f
Jirushi_I <jirushi.insignia@outlook.com>
2020-10-07 16:07:03 -0700

    Git4Unity

---
+++ Assets/Scripts/GameEventListener.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent Event;
+    public UnityEvent Response;
+
+    private void OnEnable()
+    {
+        //Check if the event exists to avoid errors
+        if (Event == null)
+        {
+            return;
+        }
+        Event.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        if (Event == null)
+        {
+            return;
+        }
+        Event.UnregisterListener(this); 
+    }
+
+    public void OnEventRaised()
+    {
+        if (Response == null)
+        {
+            return;
+        }
+        Response.Invoke(); 
+    }
+}
